72c4e2808e41eb47425c1d6c53644515b84acf1c
deepsha-3 <itsmedeepsha375@gmail.com>
2025-09-28 22:29:43 +0545

    new file added

--- Basic Project/number_to_word.py
+++ Basic Project/number_to_word.py
@@ -25,7 +25,8 @@ pos_format_lable = Label(root, text="1. Positives: ",
 
 neg_format_lable = Label(root, text="2. Negatives: ",
                fg="green", font=("Arial", 10, 'bold')).place(x=200, y=110)
-float_format_lable = Label(root, text="3. Zeros : ",
+
+float_format_lable = Label(root, text="3. Zeros: ",
                fg="green", font=("Arial", 10, 'bold')).place(x=200, y=130)
 zero_format_lable = Label(root, text="4. Floating points/decimals/fractions : ",
                fg="green", font=("Arial", 10, 'bold')).place(x=200, y=150)
