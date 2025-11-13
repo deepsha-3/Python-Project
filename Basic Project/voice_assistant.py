c141bf6e0fccf81427f33ab8b137d51014d05fa5
deepsha-3 <itsmedeepsha375@gmail.com>
2025-11-13 22:33:01 +0545

    new file added

--- Basic Project/voice_assistant.py
+++ Basic Project/voice_assistant.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
         if 'wikipedia' in query:
             speak("Searching Wikipedia ...")
             query = query.replace("wikipedia", '')
-            results = wikipedia.summary(query, sentences=2)
+            results = wikipedia.summary(query, sentences = 2)
             speak("According to wikipedia")
             speak(results)
         elif 'are you' in query:
