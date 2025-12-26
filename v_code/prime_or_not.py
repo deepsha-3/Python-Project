f1c70b2246eef80fe817273fc54e69bb2bd6baaf
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-26 20:48:45 +0545

    new file

--- v_code/prime_or_not.py
+++ v_code/prime_or_not.py
@@ -1,1 +1,9 @@
 # Write a python program using a function to check whether a given number is prime or not.
+
+def prime(num):
+    if num <1 or num == 1: 
+        return False
+    else:
+        for i in range(2, int(num**0.5)+1):
+            if num % i == 0:
+                return False
