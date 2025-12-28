248a9da2a7783b964186fad75aec30a734043e85
deepsha-3 <itsmedeepsha375@gmail.com>
2025-12-28 23:12:52 +0545

    new file added

--- v_code/prime_or_not.py
+++ v_code/prime_or_not.py
@@ -4,7 +4,7 @@ def prime(num):
     if num <1 or num == 1: 
         return False
     else:
-        for i in range(2,int(num**0.5)+1):
+        for i in range(2, int(num**0.5)+1):
             if num % i == 0:
                 return False
     return True
